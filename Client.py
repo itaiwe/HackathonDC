e4c21e63b7a4fd4f36136b79bdeed55fdf1ac122
Shalevzzz <shalevaviv13@gmail.com>
2021-12-21 22:19:01 +0200

    client check and game \ almost ready

--- Client.py
+++ Client.py
@@ -19,17 +19,40 @@ class Client:
             udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # enabling option to broadcast for ip address
             udp_socket.bind(("", self.port))
             
-            data, address = udp_socket.recvfrom(2048) ##check that
-            info = struct.unpack("Ibh", data) ##check that
+            data, address = udp_socket.recvfrom(4096) ##check that
+            info = struct.unpack("Ibh", data) ##I-unsigned int 4B b-signed char 1B h-short 2B
             tcp_ip = address[0]
             tcp_socket = self.check_offers(info, tcp_ip)
             if tcp_socket is not None:
                 self.game_mode(tcp_socket)
     
     def check_offers(self, info, tcp_ip):
-        pass
+        
+        if info[0]==self.cookie and info[1]==self.msg_type:
+            server_port=info[2]
+            print(f"Received offer from {tcp_ip} attempting to connect...")
+            tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp_socket.connect((tcp_ip, server_port))
+            massage=f"{self.name}\n".encode() ##check encode type!
+            tcp_socket.send(massage)
+            return(tcp_socket)
+        return(None)
+        
     
     def game_mode(self, tcp_socket):
+        data=tcp_socket.recv(4096)
+        print(data.decode())
+        t1=time.time()
+        while(time.time()-t1<=10):
+            if msvcrt.kbhit():
+                key=msvcrt.getch().decode()
+                if key.isnumeric():
+                    massage=key.encode()##check encode type!
+                    tcp_socket.send(massage)
+        summary=tcp_socket.recv(4096)
+        print(summary.decode())
+        tcp_socket.close()
+        print("Server disconnected, listening for offer requests...")
         pass
     
 magic_cookie, msg_type, port = 0xabcddcba, 0x2, 13117
