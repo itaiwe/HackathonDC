d8495483c8bd13af7f128e663baac036211208aa
Shalevzzz <shalevaviv13@gmail.com>
2021-12-29 21:56:03 +0200

    server colors

--- Server.py
+++ Server.py
@@ -5,7 +5,8 @@ import time
 from datetime import datetime
 import scapy.all as scapy
 import random
-
+import multiprocessing as mul_proc
+from colors import colors
 
 class server:
     def __init__(self, dev): #maybe add eth and set to eth1 or eth2 when creating server
@@ -15,13 +16,13 @@ class server:
         self.server_ip = scapy.get_if_addr("eth1" if dev else "eth2") #scapy.conf.iface
         self.tcp_port = 25000
         self.users = 0
-        self.equations = [("3+3","6"),("3+2","5"),("1+2","3"),("7+1","8"),("1+1","2")]
+        self.equations = [("3+3","6"),("3+2","5"),("1+2","3"),("7+1","8"),("1+1","2"),("9^0.5","3"),("3+10-5","8"),("(10*3-15)/5","3"),("(123*5+3)*(23-12+6^3)*(12-3*4)","0"),("|5-8|","3"),("100/10-4","6"),("5+3-2+1","7"),("(123-23-1)/11","9"),("2-1","1"),("18/3","6"),("(1/3)*9","3"),("(94/2+1)/8","6"),("13-6","7"),("45/9","5"),("-10*(-2)-19","1"),("4+3-1","6"),("3*2+1^0","7"),("0.01*100","1")]
         self.flag = False
-        self.message = ""
+        self.message = ["",""]
         self.mutex = threading.Lock()
         self.team_names = []
         self.sockets = []
-        
+
     def send_udp_offer(self, tcp_port, udp_socket:socket):
         while self.users < 2:
             format = "IbH"
@@ -35,11 +36,11 @@ class server:
         udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         udp_socket.bind((self.server_ip, self.port))
         tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # setting TCP socket
-        tcp_socket.bind((self.server_ip, self.port))
+        tcp_socket.bind((self.server_ip, self.tcp_port))
         udp_socket.settimeout(10)
         tcp_socket.settimeout(10)
+        print(colors.front.BOLD+colors.front.UNDERLINE+colors.CYAN+"Server started,"+colors.ENDC+colors.CYAN+ "listening on IP address"+colors.back.RED+colors.GREEN+f"{self.server_ip}"+colors.ENDC)
         while True:
-            print(f"Server started, listening on IP address {self.server_ip}")
             t = threading.Thread(target=self.send_udp_offer, args=(self.tcp_port, udp_socket))
             t.start()
             self.team_names, self.sockets = self.listen_tcp(tcp_socket)
@@ -49,21 +50,25 @@ class server:
             Player 1: {self.team_names[0]}Player 2: {self.team_names[1]}==
             Please answer the following question as fast as you can:
             How much is: {question}?""".encode() 
-            self.message = f'Game over!\nThe correct answer was {answer}!\n\nGame ended in a draw!'
+            for i in len(self.message):
+                self.message[i] = f'Game over!\nThe correct answer was {answer}!\n\nGame ended in a draw!'
             thread_player1 = threading.Thread(target=self.game_mode, args=(self.sockets[0], message, answer, 0))
             thread_player2 = threading.Thread(target=self.game_mode, args=(self.sockets[1], message, answer, 1))
-            thread_game=threading.Thread(target=self.count_game)
+            thread_game=mul_proc.Process(target=self.count_game)
             thread_player1.start()
             thread_player2.start()
             thread_game.start()
             thread_player1.join()
             thread_player2.join()
+            if self.flag:
+                thread_game.terminate()
             thread_game.join()
             self.sockets[0].close() #check that
             self.sockets[1].close() #check that
             self.users = 0
             self.flag = False
-            print("Game over, sending out offer requests...")
+            self.winner=["",""]
+            print(colors.front.BOLD+colors.front.UNDERLINE+colors.YELLOW+"Game over,"+colors.ENDC+colors.YELLOW+" sending out offer requests..."+colors.ENDC)
                 
             
     def listen_tcp(self, tcp_socket):
@@ -94,15 +99,17 @@ class server:
                 data = player_socket.recv(2048).decode()
                 self.mutex.acquire(1)
                 if data == answer and not self.flag:
-                    self.message = f'Game over!\nThe correct answer was {answer}!\n\nCongratulations to the winner: {self.team_names[idx]}'.strip('\n')
+                    self.message[idx] = f'Game over!\nThe correct answer was {answer}!\n\nCongratulations You Won!'
+                    self.message[abs(idx - 1)] = f'Game over!\nsadly you lost!\nThe correct answer was {answer}!\n\nCongratulations to the winner: {self.team_names[idx]}'.strip('\n')
                 elif data != answer and not self.flag:
-                    self.message = f'Game over!\nThe correct answer was {answer}!\n\nCongratulations to the winner: {self.team_names[abs(idx - 1)]}'.strip('\n')
+                    self.message[abs(idx - 1)] = f'Game over!\nThe correct answer was {answer}!\n\nCongratulations You Won!'
+                    self.message[idx] = f'Game over!\nsadly you lost!\nThe correct answer was {answer}!\n\nCongratulations to the winner: {self.team_names[idx]}'.strip('\n')
                 self.flag = True
                 self.mutex.release()
             except:
                 pass
         try:
-            player_socket.send(self.message.encode())
+            player_socket.send(self.message[idx].encode())
         except:
             pass
 
